32214446f37c4afdeb7443566c51f289fc96ba8a
ihtesham ul haq <ihtesham.ulhaq4959@gmail.com>
2023-08-23 05:33:44 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [width, setWidth] = useState(window.screen.width);
+
+  useEffect(
+    function () {
+      document.title = count;
+    },
+    [count]
+  );
+
+  useEffect(()=>{
+    window.addEventListener("resize", currentWidth);
+  })
+  const currentWidth = ()=>{
+    setWidth(()=>window.innerWidth)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <button onClick={() => setCount(count + 1)}>Increment</button>{" "}
+        <h1>{count}</h1>{" "}
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
+
+      <div className="child">
+        <h2>my current screen width is <span>{width}</span></h2>
+      </div>
+    </>
   );
 }
 
